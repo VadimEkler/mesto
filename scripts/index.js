8afcfe198eea9eb5960b864a7d92039e26fa99c1
Vadim Ekler <veklercareer@yandex.ru>
2023-06-28 14:58:23 +0700

    feature: сделать форму добавления новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,27 @@
+// Данные, необходимые для оперирования в следующих блоках
+const profileUserNickname = document.querySelector('.profile__user-nickname');
+const profileUserDescription = document.querySelector('.profile__user-description');
+
+// Все попапы
 const popup = document.querySelector('.popup');
+
+// Элементы попапа с информацией о пользователе
 const popupUserInfo = document.querySelector('.popup_user-info');
+const popupInputNickname = popupUserInfo.querySelector('.popup__form-field_input_nickname');
+const popupInputDescription = popupUserInfo.querySelector('.popup__form-field_input_description');
+const userInfoEditForm = popupUserInfo.querySelector('.popup__edit-form');
+
+// Элементы попапа с информацией о пользователе
 const popupAddImage = document.querySelector('.popup_add-image');
+const popupInputTitle = popupAddImage.querySelector('.popup__form-field_input_title');
+const popupInputLink = popupAddImage.querySelector('.popup__form-field_input_link');
 
-const openPopupBtnUserInfo = document.querySelector('.profile__edit-btn');
-const openPopupBtnAddImage = document.querySelector('.profile__add-btn');
-const closePopupBtn = document.querySelector('.popup__close-btn');
-const editForm = document.querySelector('.popup__edit-form');
+// Кнопки
+const profileEditButton = document.querySelector('.profile__edit-btn');
+const profileAddButton = document.querySelector('.profile__add-btn');
+const popupCloseButtons = document.querySelectorAll('.popup__close-btn');
 
-let userName = document.querySelector('.profile__user-nickname');
-let userDescription = document.querySelector('.profile__user-description');
-let inputUserName = editForm.querySelector('.popup__form-field_input_nickname');
-let inputUserDescription = editForm.querySelector('.popup__form-field_input_description');
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  inputUserName.setAttribute('value', `${userName.textContent}`);
-  inputUserDescription.setAttribute('value', `${userDescription.textContent}`);
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opened');
-};
-
-openPopupBtnUserInfo.addEventListener('click', function () {
-  openPopup(popupUserInfo);
-});
-
-openPopupBtnAddImage.addEventListener('click', function () {
-  openPopup(popupAddImage);
-});
-
-closePopupBtn.addEventListener('click', function () {
-  closePopup(popup);
-});
-
-function editUserInfo() {
-  userName.textContent = inputUserName.value;
-  userDescription.textContent = inputUserDescription.value;
-}
-
-function handleFormSubmit (evt) {
-  evt.preventDefault();
-  editUserInfo();
-  closePopup(popup);
-}
-
-editForm.addEventListener('submit', handleFormSubmit);
-
+// Первичный массив элементов
 const galleryItems = [
   {
     name: 'Архыз',
@@ -74,15 +49,17 @@ const galleryItems = [
   }
 ];
 
-const galleryItemTemplate = document.getElementById('gallery-item');
+// Элемент-контейнер для карточек и элемент-шаблон, с помощью которого карточки добавляем
 const galleryContainer = document.querySelector('.gallery__list');
+const galleryItemTemplate = document.querySelector('#gallery-item');
 
+// Создание карточки на основе данных, прилетающих из массива
 const createGalleryItem = (galleryItemData) => {
   const galleryItem = galleryItemTemplate.content.querySelector('.gallery__list-item').cloneNode(true);
   const galleryItemTitle = galleryItem.querySelector('.gallery__title');
   const galleryItemImage = galleryItem.querySelector('.gallery__image');
-  const galleryItemDeleteBtn = galleryItem.querySelector('.gallery__delete-button');
-  const galleryItemLikeBtn = galleryItem.querySelector('.gallery__like-button');
+  const galleryItemDeleteButton = galleryItem.querySelector('.gallery__delete-button');
+  const galleryItemLikeButton = galleryItem.querySelector('.gallery__like-button');
 
   galleryItemTitle.textContent = galleryItemData.name;
   galleryItemImage.src = galleryItemData.link;
@@ -93,19 +70,71 @@ const createGalleryItem = (galleryItemData) => {
   };
 
   const handleLike = () => {
-    galleryItemLikeBtn.classList.toggle('gallery__like-button_active');
+    galleryItemLikeButton.classList.toggle('gallery__like-button_active');
   };
 
-  galleryItemDeleteBtn.addEventListener('click', handleDelete);
+  galleryItemDeleteButton.addEventListener('click', handleDelete);
 
-  galleryItemLikeBtn.addEventListener('click', handleLike);
+  galleryItemLikeButton.addEventListener('click', handleLike);
 
   return galleryItem;
 };
 
+// Добавление каждой карточки из первичного массива в галерею
 galleryItems.forEach((item) => {
   const element = createGalleryItem(item);
 
   galleryContainer.appendChild(element);
 });
 
+// Функция открытия попапов
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+
+// Функция закрытия попапов
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// Функция изменения имени и описания пользователя
+function editUserInfo () {
+  profileUserNickname.textContent = popupInputNickname.value;
+  profileUserDescription.textContent = popupInputDescription.value;
+};
+
+
+// Отмена отправки формы на сервер, обновление данных пользователя и закрытие попапа
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  editUserInfo();
+  closePopup(popup);
+};
+
+// Сохранение информации о пользователе при сабмите формы
+userInfoEditForm.addEventListener('submit', handleFormSubmit);
+
+// Открытие попапа с инфой о пользователе при клике по иконке + автозаполенение
+profileEditButton.addEventListener('click', () => {
+  popupInputNickname.value = profileUserNickname.textContent;
+  popupInputDescription.value = profileUserDescription.textContent;
+  openPopup(popupUserInfo)
+});
+
+// Открытие попапа для добавления карточки при клике по иконке
+profileAddButton.addEventListener('click', () => openPopup(popupAddImage));
+
+// Добавление новой карточки при сабмите формы
+popupAddImage.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const galleryItemNew = {name: popupInputTitle.value, link: popupInputLink.value};
+  galleryContainer.prepend(createGalleryItem(galleryItemNew));
+  closePopup(popupAddImage);
+  evt.target.reset();
+});
+
+// Закрытие попапов при клике по иконке
+popupCloseButtons.forEach ((element) => {
+  const closestPopup = element.closest('.popup');
+  element.addEventListener('click', () => closePopup(closestPopup));
+});
